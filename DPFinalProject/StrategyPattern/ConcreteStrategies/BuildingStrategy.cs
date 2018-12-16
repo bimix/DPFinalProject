4683699e73c0e90a3e5a8e6a8c96d3c8c3f17eb1
BurimSyla <bimi.syla@bridgeit.dk>
2018-12-16 20:39:25 +0100

    strategy pattern classes (sceleton only)

---
+++ DPFinalProject/StrategyPattern/ConcreteStrategies/BuildingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPFinalProject.StrategyPattern.ConcreteStrategies
+{
+    class BuildingStrategy
+    {
+    }
+}
